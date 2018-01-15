5816b5da25902225eb94d4fe73d1399f85cfa0cb
Rafael CorRêa Alves <rafael.correa@sap.com>
2018-01-15 18:53:41 -0200

    Created attr for National Insurance Contributions

--- src/com/rca/kata/salary_slip/SalarySlip.java
+++ src/com/rca/kata/salary_slip/SalarySlip.java
@@ -3,13 +3,19 @@ package com.rca.kata.salary_slip;
 public class SalarySlip {
     private Employee employee;
     private double grossSalary;
+    private double nationalInsuranceContributions;
 
     public SalarySlip(Employee employee, double grossSalary) {
         this.employee = employee;
         this.grossSalary = grossSalary;
+        this.nationalInsuranceContributions = 0;
     }
 
     public double getGrossSalary() {
         return grossSalary;
+    }
+
+    public double getNationalInsuranceContributions() {
+        return nationalInsuranceContributions;
     }
 }
