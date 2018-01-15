6e6d1c0b6d8fe7af28d18d4a58dff33609093b9f
Rafael CorRêa Alves <rafael.correa@sap.com>
2018-01-15 17:56:22 -0200

    Created Employee class

---
+++ src/com/rca/kata/salary_slip/Employee.java
@@ -0,0 +1,11 @@
+package com.rca.kata.salary_slip;
+
+public class Employee {
+    private String name;
+    private double anualGrossSalary;
+
+    public Employee(String name, double anualGrossSalary) {
+        this.name = name;
+        this.anualGrossSalary = anualGrossSalary;
+    }
+}
