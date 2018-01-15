ed9f584629abe0a8378e2a0ae07ce2133bcd0833
Rafael CorRêa Alves <rafael.correa@sap.com>
2018-01-15 18:13:13 -0200

    Created getter method for grossSalary

--- src/com/rca/kata/salary_slip/SalarySlip.java
+++ src/com/rca/kata/salary_slip/SalarySlip.java
@@ -3,4 +3,8 @@ package com.rca.kata.salary_slip;
 public class SalarySlip {
     private Employee employee;
     private double grossSalary;
+
+    public double getGrossSalary() {
+        return grossSalary;
+    }
 }
