229e3e210cad4806c7246e6d8c3687ae15134dba
Rafael CorRêa Alves <rafael.correa@sap.com>
2018-01-15 18:11:43 -0200

    Test refactored for iteration 1

--- test/com/rca/kata/salary_slip/SalarySlipGeneratorTest.java
+++ test/com/rca/kata/salary_slip/SalarySlipGeneratorTest.java
@@ -3,19 +3,23 @@ package com.rca.kata.salary_slip;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class SalarySlipGeneratorTest {
 
     SalarySlipGenerator salarySlipGenerator;
     Employee employee;
+    SalarySlip salarySlip;
 
     @Before
     public void setUp() throws Exception {
         salarySlipGenerator = new SalarySlipGenerator();
-        employee = new Employee("",0);
+        employee = new Employee(12345,"John J Doe",5000);
     }
 
     @Test
-    public void tryToExecuteGenerateFor() throws Exception {
-        salarySlipGenerator.generateFor(employee);
+    public void whenAnualSalEqualsTo5000_grossSalEqualsTo416dot67() throws Exception {
+        salarySlip = salarySlipGenerator.generateFor(employee);
+        assertEquals(416.67,salarySlip.getGrossSalary());
     }
 }
