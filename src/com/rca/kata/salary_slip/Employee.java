bb9a4c93c810e61b176039aaf22718bf8919250e
Rafael CorRêa Alves <rafael.correa@sap.com>
2018-01-15 18:04:28 -0200

    Added id attr

--- src/com/rca/kata/salary_slip/Employee.java
+++ src/com/rca/kata/salary_slip/Employee.java
@@ -1,10 +1,12 @@
 package com.rca.kata.salary_slip;
 
 public class Employee {
+    private int id;
     private String name;
     private double anualGrossSalary;
 
-    public Employee(String name, double anualGrossSalary) {
+    public Employee(int id, String name, double anualGrossSalary) {
+        this.id = id;
         this.name = name;
         this.anualGrossSalary = anualGrossSalary;
     }
